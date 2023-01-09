4f8f15a0200ac46d401d1eebe39f25c131b7e549
Emiliano Sandri <emiliano.sandri@outlook.com>
2023-01-09 23:50:53 +0100

    Added optional parameter for export path

--- runtime/plugin/hardcopy.lua
+++ runtime/plugin/hardcopy.lua
@@ -6,9 +6,19 @@ vim.g.did_load_hardcopy = 1
 vim.api.nvim_create_user_command('Hardcopy', function(cmd)
   vim.cmd('TOhtml')
   local tempBufferNr = vim.api.nvim_get_current_buf()
-  local exportTempFilePath = vim.fn.tempname()
-  vim.cmd('wq' .. ' ' .. vim.fn.fnameescape(exportTempFilePath))
+  local exportFilePath
+  if #cmd.fargs == 0 then
+    exportFilePath = vim.fn.tempname()
+  else
+    exportFilePath = cmd.fargs[1]
+  end
+  vim.cmd('wq' .. ' ' .. vim.fn.fnameescape(exportFilePath))
   vim.cmd('bwipeout!' .. ' ' .. tempBufferNr)
-  vim.call('netrw#BrowseX', exportTempFilePath, 0)
-end, {desc = 'Dumps the buffer content into an HTML and opens the browser for viewing or printing'})
+  vim.call('netrw#BrowseX', exportFilePath, 0)
+end, {desc = 'Dumps the buffer content into an HTML and opens the browser for viewing or printing', nargs = '?'})
+
+vim.api.nvim_create_user_command('Useless', function(cmd)
+    print(#cmd.fargs)
+end, {desc = 'Test command', nargs = '?'})
+
 
