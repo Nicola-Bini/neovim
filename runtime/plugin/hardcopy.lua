f98bbe6234a31bd5102efe727befdc57473d1b8f
Emiliano Sandri <emiliano.sandri@outlook.com>
2023-01-10 23:24:28 +0100

    Extracted method export_to_html_and_open_output

--- runtime/plugin/hardcopy.lua
+++ runtime/plugin/hardcopy.lua
@@ -3,22 +3,21 @@ if vim.g.did_load_hardcopy then
 end
 vim.g.did_load_hardcopy = 1
 
-vim.api.nvim_create_user_command('Hardcopy', function(cmd)
+local function export_to_html_and_open_output(output_file_path)
   vim.cmd('TOhtml')
   local tempBufferNr = vim.api.nvim_get_current_buf()
   local exportFilePath
-  if #cmd.fargs == 0 then
-    exportFilePath = vim.fn.tempname()
+  if output_file_path then
+    exportFilePath = output_file_path
   else
-    exportFilePath = cmd.fargs[1]
+    exportFilePath = vim.fn.tempname()
   end
   vim.cmd('wq' .. ' ' .. vim.fn.fnameescape(exportFilePath))
   vim.cmd('bwipeout!' .. ' ' .. tempBufferNr)
   vim.call('netrw#BrowseX', exportFilePath, 0)
+end
+
+vim.api.nvim_create_user_command('Hardcopy', function(cmd)
+  export_to_html_and_open_output(cmd.fargs[1])
 end, {desc = 'Dumps the buffer content into an HTML and opens the browser for viewing or printing', nargs = '?'})
-
-vim.api.nvim_create_user_command('Useless', function(cmd)
-    print(#cmd.fargs)
-end, {desc = 'Test command', nargs = '?'})
-
 
